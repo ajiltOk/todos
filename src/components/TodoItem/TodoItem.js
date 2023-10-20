a2747e08d565b7da9bb89fa56b0efc7623305859
ajiltOk <21summer12@gmail.com>
2023-10-20 18:38:49 +0300

    feat: add new btn

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import styles from "../TodoItem/Todoitem.module.scss"
 import Button from "../Button/Button";
 
 export default function TodoItem( {todo, onChange, removeTodo} ) {
-    const [inEdit, setInEdit] = useState("Edit");
 
     const [todoTitle, setTodoTitle] = useState(todo.title);
 
@@ -11,9 +10,13 @@ export default function TodoItem( {todo, onChange, removeTodo} ) {
 
     const [todoValue, setTodoValue] = useState(todoTitle);
 
+    const [isEdit, setIsEdit] = useState(false);
+
+    const inputElemnt = useRef(null)
+
 
     const handleChange = (event) => {
-        setTodoTitle(event.target.value);
+        setTodoTitle(event.target.value); 
     }
 
     const handlerFocus = () => {
@@ -21,19 +24,26 @@ export default function TodoItem( {todo, onChange, removeTodo} ) {
     }
 
     const handlerBlur = () => {
-        // вот тут нужно описать, что если мы кликаем в любое место, кроме кнопки "Save", то input должен стать не активным и todoTitle стать тем, что был до редактирования или расфокусировки
+
+        setTodoTitle(todoValue)
+
+        setIsEdit(false)
+
+        setAvailability(true)
     }
 
     function changeTodo() { 
         setAvailability(false)
 
-        setInEdit("Save")
+        setIsEdit(true)
+
+        inputElemnt.current.focus()
     }
 
     function saveTodo() {
         setAvailability(true)
 
-        setInEdit("Edit")
+        setIsEdit(false)
     }
 
     let done;
@@ -45,9 +55,10 @@ export default function TodoItem( {todo, onChange, removeTodo} ) {
     return(
         <li>
             <input type="checkbox" onChange={() => onChange(todo.title)} checked={todo.completed} />
-            <input value={ todoTitle } onChange={ handleChange } onFocus={ handlerFocus} onBlur={ handlerBlur } disabled={ availability } className={done} />
+            <input ref={ inputElemnt } value={ todoTitle } onChange={ handleChange } onFocus={ handlerFocus} onBlur={ handlerBlur } disabled={ availability } className={done} />
             <div>
-                <Button buttonTitle={ inEdit } todo={ todo } changeTodo={ changeTodo } saveTodo={ saveTodo } />
+                {isEdit && <Button buttonTitle={ 'Save' } todo={ todo } saveTodo={ saveTodo } />}
+                {!isEdit && <Button buttonTitle={ 'Edit' } todo={ todo } changeTodo={ changeTodo } />}
                 <Button buttonTitle={ "Delete" } todo={ todo } removeTodo={removeTodo} />
             </div>
         </li>
